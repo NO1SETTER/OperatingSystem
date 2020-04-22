77f846189d25f6416d885468c2d99da1797d9b15
tracer-nju <tracer@nju.edu.cn>
2020-04-22 20:26:29 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 435e4c18625e4467ac9663e39208ea38   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  20:26:30 up  2:39,  1 user,  load average: 0.86, 0.46, 0.25

--- libco/co.c
+++ libco/co.c
@@ -64,6 +64,7 @@ void align_check(struct co* now)
 printf("co at %p\n",(void *)now);
 printf("co->stack at %p\n",(void *)&now->stack[STACK_SIZE-1]);
 }
+
 void co_end()//stack_switch_call的终点
 {
 printf("no %d coroutine is ended\n",current->no);
