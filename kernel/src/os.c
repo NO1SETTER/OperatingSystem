e106982f82eaad31ec2746b4d43f43485bbb47ec
tracer-nju <tracer@nju.edu.cn>
2020-04-24 20:19:17 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: bb3d8b9e99b749e0b76ebaded45e4367   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  20:19:18 up 25 min,  1 user,  load average: 0.50, 0.50, 0.42

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -4,9 +4,36 @@ static void os_init() {
   pmm->init();
 }
 
+void* allocated[1005];
+int num=0;
 static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     _putc(*s == '*' ? '0' + _cpu() : *s);
+  }
+  srand(0);
+  for(int i=0;i<1000;i++)
+  {
+    int rand_seed=rand()%5;
+    if(rand_seed!=0)//kalloc
+    {
+      int size=rand()%2048;
+      void* ptr=pmm->alloc(size);
+      #ifdef _DEBUG
+      printf("Allocated block at [%p,%p)\n",ptr,ptr+size);
+      #endif
+      allocated[num++]=ptr;
+    }
+    else//kfree
+    {
+      int r=rand()%num;
+      #ifdef _DEBUG
+      printf("Trying to free %p\n",allocated[r]);
+      #endif
+      pmm->free(allocated[r]);
+      #ifdef _DEBUG
+      printf("Successfully freed\n");
+      #endif
+    }
   }
   while (1) ;
 }
@@ -15,3 +42,5 @@ MODULE_DEF(os) = {
   .init = os_init,
   .run  = os_run,
 };
+
+
