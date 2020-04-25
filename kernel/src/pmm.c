cea53c3926f36e2bd00c503eeb1e5a41c63c570e
tracer-nju <tracer@nju.edu.cn>
2020-04-25 10:17:42 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: b4eef4a333864c03bf96db57d43f5567   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  10:17:42 up 13 min,  1 user,  load average: 0.63, 0.56, 0.50

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -170,7 +170,6 @@ static void *kalloc(size_t size) {
       assert(alloc_blk);
       //printf("alloc->start=%p\n",alloc_blk->start);
       alloc_blk->start=valid_addr;
-      assert(0);
       alloc_blk->end=valid_addr+size;
       alloc_blk->size=size;
       binsert(alloc_head,alloc_blk,0);
