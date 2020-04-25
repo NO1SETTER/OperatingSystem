58b4816af06809bd381a0ef8ce3aae29765eb8f0
tracer-nju <tracer@nju.edu.cn>
2020-04-25 15:38:43 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: f6e7612145684cab8897547155623d69   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  15:38:44 up  2:13,  1 user,  load average: 0.76, 0.81, 0.68

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -46,6 +46,7 @@ static void os_run() {
     }
     print_FreeBlock();
     print_AllocatedBlock();
+    printf("DISPLAYED DISPLAYED DISPLAYED DISPLAYED DISPLAYED DISPLAYED\n");
   }
   while (1) ;
 }
