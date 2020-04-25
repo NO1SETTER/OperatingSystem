dc44114adeaee31666223ba4ae953b33a99d488d
tracer-nju <tracer@nju.edu.cn>
2020-04-25 12:59:22 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: b4eef4a333864c03bf96db57d43f5567   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  12:59:23 up  2:55,  1 user,  load average: 0.58, 0.60, 0.56

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -5,15 +5,18 @@ typedef struct
   intptr_t locked;
 }lock_t;
 
+
 struct block//管理空闲块或非空闲块的数据结构
 {
-uintptr_t start,end;//[start,end)
+uintptr_t start,end;//管理[start,end)
 uint32_t size;
 struct block* prev;
 struct block* next;
 lock_t lk;
 };
 
+static void *balloc();
+static void bfree(struct block* blk);
 struct block* free_head;
 struct block* alloc_head;//两个都是空的节点
 
@@ -28,13 +31,11 @@ void sp_lock(lock_t* lk)
   while(_atomic_xchg(&lk->locked,1))
   {            }
 }
-
 void sp_unlock(lock_t *lk)
 {
   if(lk==NULL) return;
   _atomic_xchg(&lk->locked,0);
 }
-
 void block_init(struct block *blk)
 {
   sp_lockinit(&blk->lk);}
@@ -61,10 +62,7 @@ void blink(struct block* pre,struct block*nxt)//直接连接
 }
 
 void bdelete(struct block* blk)//删除
-{
-assert(blk->prev);
-
-if(blk->next)
+{if(blk->next)
 {
 (blk->prev)->next=blk->next;
 (blk->next)->prev=blk->prev;
@@ -97,7 +95,7 @@ void binsert(struct block* pre,struct block* nxt,bool is_merge)//插入
       {
         pre->end=nxt->end;
         pre->size=pre->end-pre->start;
-        free(nxt);
+        bfree(nxt);
       }
       else
       binsert(pre,nxt,0);
@@ -110,18 +108,18 @@ void binsert(struct block* pre,struct block* nxt,bool is_merge)//插入
       {//三合一
       ptr1->end=ptr2->end;
       ptr1->size=ptr1->end-ptr1->start;
-      free(nxt);
-      free(ptr2);
+      bfree(nxt);
+      bfree(ptr2);
       }
       else if(ptr1->end==nxt->start)
       {ptr1->end=nxt->end;
       ptr1->size=ptr1->end-ptr1->start;
-      free(nxt);
+      bfree(nxt);
       }
       else if(nxt->end==ptr2->start)
       {ptr2->start=nxt->start;
       ptr2->size=ptr2->end-ptr2->start;
-      free(nxt);
+      bfree(nxt);
       }
       else//两不沾
       binsert(ptr1,nxt,0);
@@ -129,7 +127,6 @@ void binsert(struct block* pre,struct block* nxt,bool is_merge)//插入
 
   //is_merge代表连成一段的直接合并
 }
-
 
 void print_FreeBlock()
 {
@@ -143,7 +140,6 @@ void print_FreeBlock()
   }
   #endif
 }
-
 
 void print_AllocatedBlock()
 {
@@ -169,6 +165,37 @@ uint32_t ret=(((uint32_t)(start-1)/temp)+1)*temp;
 return (uintptr_t)ret;
 }
 
+const int max_block_num=0x2000000/sizeof(struct block);
+//最多给管理的块分配0x2000000的空间
+struct SET
+{
+  int s[20000];
+  int size;
+}mset;//记录被释放后的块的位置，方便直接使用
+void spush(int x)
+{mset.s[mset.size++]=x;}
+int sfindpos()//只是要一个位置,无所谓分到哪一个
+{return mset.s[--mset.size];}
+int maxpos=0;//当前已经分配到的最大位置，当mset为空时从这里开始分配
+uintptr_t bstart;
+static void *balloc()//专门给block分配空间用,直接从某一位置开始往上垒不用对齐
+{
+  if(mset.size)
+  {
+    int no=sfindpos();
+    maxpos=max(maxpos,no);
+    return (void *)(bstart+no*sizeof(struct block));
+  }
+  uintptr_t ret=(uintptr_t)(bstart+maxpos*sizeof(struct block));
+  maxpos=maxpos+1;
+  return (void *)ret;
+}
+
+static void bfree(struct block* blk)
+{
+  int no =((uintptr_t)blk-bstart)/sizeof(struct block);
+  spush(no);
+}
 
 static void *kalloc(size_t size) {
     struct block*ptr=free_head->next;
@@ -188,7 +215,7 @@ static void *kalloc(size_t size) {
     {
       ptr->start=valid_addr+size;
       ptr->size=ptr->end-ptr->start;
-      struct block *alloc_blk=(struct block*)malloc(sizeof(struct block));
+      struct block *alloc_blk=(struct block*)balloc(sizeof(struct block));
       assert(alloc_blk);
       alloc_blk->start=valid_addr;
       alloc_blk->end=valid_addr+size;
@@ -200,7 +227,7 @@ static void *kalloc(size_t size) {
     {
       ptr->end=valid_addr;
       ptr->end=ptr->end-ptr->start;
-      struct block *alloc_blk=(struct block*)malloc(sizeof(struct block));
+      struct block *alloc_blk=(struct block*)balloc(sizeof(struct block));
       alloc_blk->start=valid_addr;
       alloc_blk->end=valid_addr+size;
       alloc_blk->size=size;
@@ -209,8 +236,8 @@ static void *kalloc(size_t size) {
     }
     else
     {
-      struct block*alloc_blk=(struct block*)malloc(sizeof(struct block));
-      struct block*free_blk=(struct block*)malloc(sizeof(struct block));
+      struct block*alloc_blk=(struct block*)balloc(sizeof(struct block));
+      struct block*free_blk=(struct block*)balloc(sizeof(struct block));
       free_blk->end=ptr->end;
       free_blk->start=valid_addr+size;
       free_blk->size=free_blk->end-free_blk->start;
@@ -269,17 +296,19 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)_heap.end - (uintptr_t)_heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
-  free_head=(struct block *)malloc(sizeof(struct block));
-  alloc_head=(struct block *)malloc(sizeof(struct block));
+  free_head=(struct block *)balloc(sizeof(struct block));
+  alloc_head=(struct block *)balloc(sizeof(struct block));
   free_head->start=free_head->end=free_head->size=0;
   alloc_head->start=alloc_head->end=alloc_head->size=0;
 
-  struct block *blk=(struct block *)malloc(sizeof(struct block));
+  struct block *blk=(struct block *)balloc(sizeof(struct block));
   blk->start=(uintptr_t)_heap.start;
   blk->end=(uintptr_t)_heap.end;
   blk->size=blk->end-blk->end;
   blk->prev=free_head;
   free_head->next=blk;
+  mset.size=0;
+  bstart=(uintptr_t)_heap.end-0x2000000;
 }
 
 MODULE_DEF(pmm) = {
