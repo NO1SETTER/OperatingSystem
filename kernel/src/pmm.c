b06bbddba165a33a10a98425f2b1f70129ea7f8d
tracer-nju <tracer@nju.edu.cn>
2020-04-27 20:23:03 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 318e3ab66d1b4e99a287745a193ddad5   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  20:23:03 up  4:03,  1 user,  load average: 0.45, 0.28, 0.27

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -36,8 +36,8 @@ void sp_lock(lock_t* lk)
   while(_atomic_xchg(&lk->locked,1))
   {
     //assert(lk->name);
-    //printf("CPU#%d Acquiring lock for %s\n",_cpu(),lk->name);
   }
+      printf("CPU#%d Acquires lock for %s\n",_cpu(),lk->name);
 }
 void sp_unlock(lock_t *lk)
 {
