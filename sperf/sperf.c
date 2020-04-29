37180ea72fa39eab9df963a314af5d5d286d2a93
tracer-nju <tracer@nju.edu.cn>
2020-04-29 15:32:35 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: e481dc989a844f429f2180ce3a8df885   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  15:32:35 up 16 min,  1 user,  load average: 1.61, 1.27, 0.89

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -15,13 +15,16 @@ char *exec_argv[10];//最多传十个参数
 char exec_envp[100][1000];
 int arg_num;
 int env_num;
+char strace_path[50];
 
 void parse_args_envp(int argc,char **argv);
 void print_message();
-
+void find_strace_path();
 int main(int argc, char *argv[]) {
   parse_args_envp(argc,argv);
   print_message();
+  find_strace_path();
+  printf("strace at %s\n",strace_path);
   assert(0);
   /*char *exec_argv[] = { "strace", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
@@ -89,4 +92,56 @@ void print_message()
   printf("ENV:\n");
   for(int i=0;i<env_num;i++)
   printf("env[%d]:%s\n",i,exec_envp[i]);
+}
+
+int get_strace=0;
+void read_all_file(char *basepath)//寻找strace,找到返回1，否则返回0
+{
+  DIR* dir;
+  struct dirent* ptr;
+  char base[200];
+  strcpy(base,basepath);
+  
+  assert(dir=opendir(basepath));
+  while((ptr=readdir(dir))!=NULL)
+  {
+    if(get_strace) break;
+    if(strcmp(ptr->d_name,".")==0||strcmp(ptr->d_name,"..")==0)
+    continue;
+    if(ptr->d_type!=4)
+    {
+      if(strcmp(ptr->d_name,"strace")==0)//找到
+      {
+      strcat(base,"/");
+      strcat(base,ptr->d_name);
+      strcpy(strace_path,basepath);
+        get_strace=1;
+      }
+    }
+    else
+    {
+      char base[100];
+      strcat(base,"/");
+      strcat(base,ptr->d_name);
+      read_all_file(base,name);
+    }
+  }
+  closedir(dir);
+}
+
+void find_strace_path()//找到执行程序的路径,把它写到exec_path里去
+{
+char basepath[100];
+memset(basepath,0,sizeof(basepath));
+getcwd(basepath,sizeof(basepath));
+if(get_strace) return;
+
+get_strace=0;
+for(int i=0;i<n;i++)
+{
+  strcpy(basepath,exec_envp[i]);
+  find_strace_path(basepath);
+  if(get_strace) break;
+  get_strace=0;
+}
 }
