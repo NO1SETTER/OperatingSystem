cf662b6016e0483f735ddf78f444ae79a849b113
tracer-nju <tracer@nju.edu.cn>
2020-04-29 10:41:20 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 079fa5d3a90148a4bdf7e11abb2b6b95   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  10:41:20 up  1:27,  1 user,  load average: 0.68, 0.43, 0.43

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -37,7 +37,7 @@ exec_envp[0] = strtok(path,":");
 printf("%s\n",exec_envp[0]);
 assert(0);
 char *s;
-for(int i=1;(s=strtokassert(0);(NULL,":"))!=NULL;i++)
+for(int i=1;(s=strtok(NULL,":"))!=NULL;i++)
 {
   exec_envp[i]=s;
 }
