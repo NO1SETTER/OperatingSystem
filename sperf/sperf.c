9e733435880531d21765261c6578a5fb6619a26d
tracer-nju <tracer@nju.edu.cn>
2020-04-29 15:17:44 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: e481dc989a844f429f2180ce3a8df885   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-96-generic       Architecture: x86-64  15:17:44 up 2 min,  1 user,  load average: 1.76, 0.98, 0.38

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -10,10 +10,7 @@
 extern char **environ;
 
 
-char *path;
-char *command;
-char exex_path[100];//执行程序路径
-
+char *path;//path环境变量
 char *exec_argv[10];//最多传十个参数
 char *exec_envp[1000];
 int arg_num;
@@ -48,7 +45,6 @@ int main(int argc, char *argv[]) {
   }
   else//parent writes to pipefd[1]
   {
-    find_path();
   }
 
 
@@ -58,12 +54,12 @@ int main(int argc, char *argv[]) {
 
 void parse_args_envp(int argc,char **argv)//把参数环境变量什么的都解析了
 {
-command=argv[1];
-for(int i=2;i<argc;i++)
-{exec_argv[i-2]=argv[i];
-}
-exec_argv[argc-1]=NULL;
-arg_num=argc-2;
+exec_argv[0]="strace";
+for(int i=1;i<argc;i++)
+exec_argv[i]=argv[i];
+exec_argv[argc]=NULL;()
+arg_num=argc;
+
 char **ptr=environ;
   while(*ptr)
   {
@@ -77,10 +73,13 @@ char **ptr=environ;
 
 strtok(path,"=");
 char *s;
+char temp[200]="PATH=";
 int pos=0;
   for(;(s=strtok(NULL,":"))!=NULL;pos++)
   {
-    exec_envp[pos]=s;
+    strcat(temp,s);
+    exec_envp[pos]=temp;
+    strcpy(temp,"PATH=");
   }
 env_num=pos;
 }
@@ -94,50 +93,4 @@ void print_message()
   printf("ENV:\n");
   for(int i=0;i<env_num;i++)
   printf("env[%d]:%s\n",i,exec_envp[i]);
-}
-
-int getfile=0;
-void read_all_file(char *basepath,char name)//寻找某个文件夹下某个文件,找到返回1，否则返回0
-{
-  DIR* dir;
-  struct dirent* ptr;
-  char base[200];
-  strcpy(base,basepath);
-  
-  assert(dir=opendir(basepath));
-  while((ptr=readdir(dir))!=NULL)
-  {
-    if(strcmp(ptr->d_name,".")==0||strcmp(ptr->d_name,"..")==0)
-    continue;
-    if(ptr->d_type!=4)
-    {
-      if(strcmp(ptr->d_name,name)==0)//找到
-      {
-      strcat(base,"/");
-      strcat(base,ptr->d_name);
-        strcpy(exec_path,basepath);
-        getfile=1;
-      }
-    }
-    else
-    {
-      char base[100];
-      strcat(base,"/");
-      strcat(base,ptr->d_name);
-      read_all_file(base,name);
-    }
-  }
-  closedir(dir);
-}
-
-void find_path()//找到执行程序的路径,把它写到exec_path里去
-{
-if(command[0]=='/')//给出绝对路径的情况
-{
-  strcmp(exex_path,command);
-  return;
-}
-char basepath[100];
-memset(basepath,0,sizeof(basepath));
-getcwd(basepath,sizeof(basepath));
 }
