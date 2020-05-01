dfec0e3409aea59ddb5a1d7025d5e745c82215c4
tracer-nju <tracer@nju.edu.cn>
2020-05-01 19:27:26 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 9899c71999a14132bdef2ce7f3c221f7   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-99-generic       Architecture: x86-64  19:27:26 up  1:14,  1 user,  load average: 1.03, 0.47, 0.33

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -52,22 +52,18 @@ int main(int argc, char *argv[]) {
   if(cpid==0)//child reads from pipefd[0]
   {
     close(pipefd[1]);
-    printf("\n\nChild file descriptor turned from %d to",pipefd[0]);
     int ret=dup2(pipefd[0],STDIN_FILENO);
     assert(ret==STDIN_FILENO);
-    printf("%d\n\n",ret);
     char buf;
     //while(read(pipefd[0],&buf,1)>0)
     //{printf("READ %c ",buf);}
   }
   else//parent writes to pipefd[1]
   {
-    //close(pipefd[0]);
-    printf("\n\nParent file descriptor turned from %d to",pipefd[0]);
+    close(pipefd[0]);
     int ret=dup2(pipefd[1],STDOUT_FILENO);
     assert(ret==STDOUT_FILENO);
-    printf("%d\n\n",ret);
-    //execve(strace_path,exec_argv,exec_env);
+    execve(strace_path,exec_argv,exec_env);
   }
 
 
