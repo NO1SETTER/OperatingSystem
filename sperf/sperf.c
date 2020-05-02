afcd13e9674c40702e5bfb34f445101d0aced94c
tracer-nju <tracer@nju.edu.cn>
2020-05-02 13:09:58 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 2cc9048f4bc041b88eb9ba7f3834cb25   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-99-generic       Architecture: x86-64  13:09:58 up  3:27,  1 user,  load average: 1.86, 1.19, 0.81

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
       {
         buffer[len]='\0';//读取了一行的数据,进行分析
         if(buffer[0]=='+') break;
-        printf("%s\n",buffer);
+        //printf("%s\n",buffer);
         char name[50];
         char tstr[20];
         memset(name,0,sizeof(name));
@@ -115,7 +115,7 @@ int main(int argc, char *argv[]) {
         else 
         t=atof(tstr);
         total=total+t;
-        printf("name=%s t=%f\n\n",name,t);
+        //printf("name=%s t=%f\n\n",name,t);
         int rec=0;
         for(int i=0;i<sys_num;i++)
         {
