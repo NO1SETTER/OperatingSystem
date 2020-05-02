c53b28c208f1b21f9844684678e152356058f575
tracer-nju <tracer@nju.edu.cn>
2020-05-02 09:52:19 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 2cc9048f4bc041b88eb9ba7f3834cb25   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-99-generic       Architecture: x86-64  09:52:18 up 10 min,  1 user,  load average: 1.36, 1.03, 0.62

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -55,8 +55,8 @@ int main(int argc, char *argv[]) {
     int ret=dup2(pipefd[0],STDIN_FILENO);
     assert(ret==STDIN_FILENO);
     char buf;
-    while(read(pipefd[0],&buf,1)>0)
-    {printf("%c",buf);}
+    //while(read(pipefd[0],&buf,1)>0)
+    //{printf("%c",buf);}
   }
   else//parent writes to pipefd[1]
   {
