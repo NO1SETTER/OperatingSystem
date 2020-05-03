ac78efe1136bd0594dcb964bed25bc3c52b6875e
tracer-nju <tracer@nju.edu.cn>
2020-05-03 09:59:41 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 8d4b4edb235b418e80634c2833fbd47b   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-99-generic       Architecture: x86-64  09:59:41 up 29 min,  1 user,  load average: 0.41, 0.44, 0.50

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -159,7 +159,7 @@ int main(int argc, char *argv[]) {
       sysctrl[i].ratio=(int)(100*sysctrl[i].t/total);
       printf("%s(%d%%)\n",sysctrl[i].name,sysctrl[i].ratio);
     }
-    //fflush(stdout);
+    fflush(stdout);
     total=0;
     for(int i=0;i<sys_num;i++)//统计后清零
       sysctrl[i].t=0;
