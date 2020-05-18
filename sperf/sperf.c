910955de8fe4e17e219a438edef210b9ba2a6b9b
tracer-nju <tracer@nju.edu.cn>
2020-05-18 14:32:02 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 308dcdb90f9b4f11b5b024363968fba0   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-99-generic       Architecture: x86-64  14:32:02 up  2:03,  1 user,  load average: 1.13, 0.98, 0.79

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -240,7 +240,8 @@ void read_all_file(char *basepath)//寻找strace,找到返回1，否则返回0
   if((dir=opendir(basepath))==NULL)//这里失败了
   {
     printf("Failed opening %s\n",basepath);
-    //assert(0);
+    printf("errno is %d\n",errno);
+    assert(0);
   }
 
   char base[200];
@@ -262,6 +263,7 @@ void read_all_file(char *basepath)//寻找strace,找到返回1，否则返回0
     }
     else
     {
+      char base[200];
       strcat(base,"/");
       strcat(base,ptr->d_name);
       read_all_file(base);
