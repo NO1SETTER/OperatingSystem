cf345595267007f0ca1761de442a6bc10ec6c87a
tracer-nju <tracer@nju.edu.cn>
2020-05-21 15:49:17 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 0c634f395e1c4c04b51af890108bca7d   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  15:49:17 up  6:46,  1 user,  load average: 1.49, 1.07, 1.02

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -23,21 +23,24 @@ int num=0;
 
 static void test1();
 static void test2();
+static void test3();
 static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     _putc(*s == '*' ? '0' + _cpu() : *s);
   }
   
-  int sel=1;
+  int sel=3;
   if(sel==1)
   test1();
   else if(sel==2)
   test2();
+  else if(sel==3)
+  test3();
 
   while (1) ;
 }
 
-static void test1()
+static void test1()//在[0,2048)完全随机
 { printf("Conducting test1\n"); 
     for(int i=0;i<1000;i++)
   { 
@@ -80,7 +83,7 @@ static void test1()
   }
 }
 
-static void test2()
+static void test2()//交替测试大内存和小内存
 { printf("Conducting test2\n");
   
   for(int i=0;i<1000;i++)//小内存大内存交替分配释放
@@ -127,6 +130,51 @@ static void test2()
     printf("Finishing Round %d for CPU#%d\n",i,_cpu());
   }
 }
+
+static void test3()//频繁分配小内存
+{ printf("Conducting test3\n");
+  
+  for(int i=0;i<10000;i++)//小内存大内存交替分配释放
+  {
+    printf("Round %d for CPU#%d\n",i,_cpu());
+    int rand_seed=rand()%5;
+    if(rand_seed!=0)//kalloc
+    {
+      int size=rand()%128;
+      if(size==0) continue;
+      #ifdef _DEBUG
+      sp_lock(&print_lock);
+      printf("Allocating size %d\n",size);
+      sp_unlock(&print_lock);
+      #endif
+      void* ptr=pmm->alloc(size);
+      #ifdef _DEBUG
+      sp_lock(&print_lock);
+      printf("Allocated block of size %d at [%p,%p) for CPU#%d\n",size,ptr,ptr+size,_cpu());
+      sp_unlock(&print_lock);
+      #endif
+      allocated[num++]=ptr;
+    }
+    else//kfree
+    {
+      if(num==0) continue;
+      int r=rand()%num;
+      #ifdef _DEBUG
+      sp_lock(&print_lock);
+      printf("Trying to free %p for CPU#%d\n",allocated[r],_cpu());
+      sp_unlock(&print_lock);
+      #endif
+      pmm->free(allocated[r]);      
+      #ifdef _DEBUG
+      sp_lock(&print_lock);
+      printf("Successfully freed\n");
+      sp_unlock(&print_lock);
+      #endif
+    }
+    printf("Finishing Round %d for CPU#%d\n",i,_cpu());
+  }
+}
+
 MODULE_DEF(os) = {
   .init = os_init,
   .run  = os_run,
