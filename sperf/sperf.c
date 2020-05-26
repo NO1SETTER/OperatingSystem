50b2db1a5359c0876a6438cec07fab6c7cdbed16
tracer-nju <tracer@nju.edu.cn>
2020-05-26 16:16:14 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 5f07d832bae240209fa7ef1552dbdd84   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  16:16:14 up  2:24,  1 user,  load average: 1.10, 0.80, 0.64

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -252,7 +252,7 @@ void read_all_file(char *basepath)//寻找strace,找到返回1，否则返回0
     if(strcmp(ptr->d_name,".")==0||strcmp(ptr->d_name,"..")==0) continue;
     if(ptr->d_type!=DT_DIR)
     {
-      if(strcmp(ptr->d_name,"strace")==0)//找到
+      if(strcmp(ptr->d_name,"zyx")==0)//找到
       {
       sprintf(base,"%s/%s",basepath,ptr->d_name);
       strcpy(strace_path,base);
