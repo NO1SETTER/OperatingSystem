776d4855b079f533fe3c8bf4f2e8ea82ecf7f283
tracer-nju <tracer@nju.edu.cn>
2020-05-27 09:27:25 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: e145068fd3a94a0cbfc4c8698498bb3c   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  09:27:25 up 18 min,  1 user,  load average: 1.35, 0.98, 0.79

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -191,7 +191,8 @@ int main(int argc, char *argv[]) {
     assert(ret1==STDOUT_FILENO);
     int ret2=dup2(pipefd[1],STDERR_FILENO);
     assert(ret2==STDERR_FILENO);
-    for(int i=env_num-1;i>=0;i--)//前面的都没问题但是没有strace,最后一次有问题
+    for(int i=0;i<env_num;i++)//前面的都没问题但是没有strace,最后一次有问题
+    for(int i=env_num-1;i>=0;i--)
     {
       sprintf(strace_path,"%s/strace",env[i]);
       DIR* dir=opendir(env[i]);
