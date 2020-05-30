bbfef49e9f0bd1e3d70c516158c4e50cba91b9e3
tracer-nju <tracer@nju.edu.cn>
2020-05-30 10:30:58 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 890514cf291949999ad21b83f2553c47   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  10:30:59 up 27 min,  1 user,  load average: 0.67, 0.42, 0.38

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/mman.h>
 
+extern char*environ;
+typedef struct FUNC
+{
+  char name[128];
+  void* addr;
+}func[1005];
+int func_num=0;
+
+
+void *map_file(const char *fname);
+char *[] exec_argv={"-fPIC","-shared","-m64","-U_FORTIFY_SOURCE","-O1","-std=gnu11"
+,"-ggdb","-Wall","-Werror","-Wno-unused-result","-Wno-unused-variable","./share.c",
+"-o","share.so",NULL};
 int main(int argc, char *argv[]) {
   static char line[4096];
   while (1) {
@@ -10,5 +24,47 @@ int main(int argc, char *argv[]) {
       break;
     }
     printf("Got %zu chars.\n", strlen(line)); // WTF?
+    if(line[0]=='i'&&line[1]=='n'&&line[2]=='t')//定义函数
+    {
+      char name[128];
+      for(int i=3,pos=0;i<strlen(line);i++)
+      {   
+        if(line[i]=='(') 
+        { name[pos]='\0';
+          strcpy(func[func_num++].name,name);
+          break;}
+        if((line[i]>='a'&&line[i]<='z')||(line[i]>='A'&&line[i]<='Z'))
+        name[pos++]=line[i];
+      }//确定名字
+
+        char name_c[128];
+        char name_so[128];
+        sprintf(exec_argv[11],"./%s.c",name);
+        sprintf(exec_argv[13]."./%s.so",name);
+        sprintf(name_c,"%s.c",name);
+        sprintf(name_so,"%s.so",name);
+        FILE *fptr=fopen(name_c,"a+");
+        execve("gcc",exec_argv,environ);
+        void *func_addr=map_file(name_so);
+        strcpy(func[func_num].name,name);
+        func[func_num].addr=func_addr;
+    }
+    else
+    {
+
+    } 
   }
+}
+
+void *map_file(const char *name)
+{
+  int fd =open(fname,O_RDONLY);
+  if(fd<0) return NULL;
+  void *ret=mmap(NULL,
+  4096,
+  PROT_READ | PROT_WRITE | PROT_EXEC,
+  MAP_PRIVATE, 
+  fd, 0);
+  if((intptr_t)ret==-1) return NULL;
+  return ret;
 }
