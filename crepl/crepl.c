a98310b9566999149b29e93b0c610d16d800262d
tracer-nju <tracer@nju.edu.cn>
2020-05-30 12:58:43 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 890514cf291949999ad21b83f2553c47   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  12:58:44 up  2:54,  1 user,  load average: 1.21, 0.81, 0.63

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -16,6 +16,11 @@ struct FUNC
 }func[1005];
 int func_num=0;
 
+cahr env_path[1000];
+char env[200][1000];
+int env_num;
+char gcc_path[200];
+
 char * exec_argv[100]={"gcc","-fPIC","-shared","-m64","-U_FORTIFY_SOURCE","-O1","-std=gnu11"
 ,"-ggdb","-Wall","-Werror","-Wno-unused-result","-Wno-unused-variable","./share.c",
 "-o","share.so",NULL};
@@ -67,7 +72,7 @@ void recursive_handle()
         FILE *fptr=fopen(name_c,"a+");
         fprintf(fptr,"%s",line);
         fclose(fptr);
-        execve("gcc",exec_argv,environ);//这里只能做到编译成共享库,记录，加载都要在父进程中进行
+        execve(gcc_path,exec_argv,environ);//这里只能做到编译成共享库,记录，加载都要在父进程中进行
         perror("After execve:gcc");
       }
     }
@@ -77,6 +82,46 @@ void recursive_handle()
     } 
 }
 
+void parse_args_envp(int argc,char **argv);
+void find_gcc_path();
 int main(int argc, char *argv[]) {
+parse_args_envp(argc,argv);
+find_gcc_path();
 recursive_handle();
+}
+
+void parse_args_envp(int argc,char **argv)//把环境变量解析
+{
+  char *path=getenv("PATH");
+  sprintf(env_path,"PATH=%s",path); 
+  strtok(env_path,"=");
+  char *s;
+  int pos=0;
+    for(;(s=strtok(NULL,":"))!=NULL;pos++)
+    {
+      sprintf(env[pos],"%s",s);
+      //printf("length of env[%d]:%s is %u\n",pos,env[pos],(unsigned)strlen(env[pos]));
+    }
+  env_num=pos;
+}
+
+void find_gcc_path()//找到执行程序的路径,把它写到exec_path里去
+{
+  char basepath[200];
+  for(int i=0;i<env_num;i++)//test env_num=8
+  {
+    sprintf(basepath,"%s",env[i]);
+    DIR* dir=opendir(basepath);
+    if(dir==NULL) continue;
+    struct dirent* ptr;
+    while((ptr=readdir(dir))!=NULL)
+    {       
+      if(strcmp(ptr->d_name,"gcc")==0)
+      {
+        sprintf(gcc_path,"%s/gcc",basepath);
+        return;
+      }
+    }
+  }
+  return;
 }
