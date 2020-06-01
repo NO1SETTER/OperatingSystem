60213ce5dda7130f10206f5f29afe8bdbec2a67c
tracer-nju <tracer@nju.edu.cn>
2020-06-01 21:37:30 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 791ede2c52374d558c3ba67f3db74a67   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  21:37:30 up  4:18,  1 user,  load average: 0.31, 0.50, 0.52

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -27,12 +27,12 @@ static void os_init() {
 #ifdef DEBUG_LOCAL
   kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
   kmt->sem_init(&fill,  "fill",  0);
-  assert(0);
   for (int i = 0; i < 4; i++) // 4 个生产者
     kmt->create(task_alloc(), "producer", producer, NULL);
   for (int i = 0; i < 5; i++) // 5 个消费者
     kmt->create(task_alloc(), "consumer", consumer, NULL);
 #endif
+assert(0);
 }
 
 void sp_lockinit(struct spinlock_t* lk,const char *name)
