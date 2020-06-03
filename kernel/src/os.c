a9cfc50de94a828a989308d0226a34049344849a
tracer-nju <tracer@nju.edu.cn>
2020-06-03 17:08:48 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: d537c6dd9cb7468eb472f06bfbac7e17   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  17:08:48 up 31 min,  1 user,  load average: 0.61, 0.54, 0.46

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -594,17 +594,9 @@ static void sem_wait(struct sem_t *sem)
     {
     current->next=sem->waiter->next;
     sem->waiter->next=current;}
-    struct task_t* ptr=sem->waiter;
-    printf("\n%s waiter :",sem->name);
-    while(ptr)
-    {
-      printf("%s ",ptr->name);
-      ptr=ptr->next;
-    } 
-    printf("\n\n");
     _yield();
     return;
-  }
+    }
 kmt->spin_unlock(&sem->lock);
 }
 
@@ -619,13 +611,6 @@ static void sem_signal(struct sem_t *sem)
       struct task_t *ptr = sem->waiter;
       sem->waiter=sem->waiter->next;//为了简单直接选取第一个activate
       activate(ptr);//这一部分是弄到active_thread中去
-          printf("\n%s waiter :",sem->name);
-    while(ptr)
-    {
-      printf("%s ",ptr->name);
-      ptr=ptr->next;
-    } 
-    printf("\n\n");
     }
   kmt->spin_unlock(&sem->lock);
 }
