faecc594d58a430cbe33bd01609b20ab66c11fbe
tracer-nju <tracer@nju.edu.cn>
2020-06-03 17:45:24 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: d537c6dd9cb7468eb472f06bfbac7e17   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  17:45:24 up  1:08,  1 user,  load average: 0.22, 0.27, 0.22

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -486,6 +486,7 @@ MODULE_DEF(os) = {
 void activate(struct task_t* t)//wait->running
 {
   sp_lock(&thread_ctrl_lock);
+  t->next=NULL;
   printf("%s trying activated\n",t->name);
   int pos=-1;
   for(int i=0;i<wait_num;i++)
@@ -617,7 +618,6 @@ static void sem_signal(struct sem_t *sem)
   printf(" sem_signal:%s val=%d\n",sem->name,sem->val);
     if(sem->waiter)
     {
-
       struct task_t *ptr = sem->waiter;
       sem->waiter=sem->waiter->next;//为了简单直接选取第一个activate
       activate(ptr);//这一部分是弄到active_thread中去
