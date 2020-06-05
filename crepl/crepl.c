e134974b2810af20bd1e63263fda14fe70ca5126
tracer-nju <tracer@nju.edu.cn>
2020-06-05 22:40:51 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 9ee2f99fdb16480d8ef647e87256b544   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  22:40:52 up  1:46,  1 user,  load average: 1.04, 0.78, 0.71

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -272,7 +272,7 @@ int getfuncret(char *s)//s[l,e)
     if(cpid!=0)
     {
         void *func_handler;
-        while((func_handler=dlopen(name_so,RTLD_LAZY|RTLD_GLOBAL))==NULL);//保证编译完才加载
+        while((func_handler=dlopen(name_so,RTLD_NOW|RTLD_GLOBAL))==NULL);//保证编译完才加载
         int (*func_addr)();
         while((func_addr=(int(*)())dlsym(func_handler,name_func))==NULL);//确保函数加载完成
         return (int)(*func_addr)();
@@ -373,7 +373,7 @@ void recursive_handle()
       if(cpid!=0)//这一部分完成加载，保存
       {
         void *func_handler;
-        while((func_handler=dlopen(name_so,RTLD_LAZY|RTLD_GLOBAL))==NULL);//保证编译完才加载
+        while((func_handler=dlopen(name_so,RTLD_NOW|RTLD_GLOBAL))==NULL);//保证编译完才加载
         void *func_addr;
         while((func_addr=dlsym(func_handler,name))==NULL);//确保函数加载完成
         dlclose(func_handler);
