b6f6f889c4bcd8d66adb5d0a738fff3f2ae31005
tracer-nju <tracer@nju.edu.cn>
2020-06-07 13:42:44 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: f999985a5309478ca2aae7c0ea62d336   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  13:42:45 up  5:13,  1 user,  load average: 1.02, 0.54, 0.59

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -3,12 +3,12 @@
 #include<string.h>
 #include<stdint.h>
 #include<assert.h>
-typedef struct 
+struct 
 {
-uint8_t BS_jmpBoot[3];
-uint8_t BS_OEMName[8];
-uint32_t BPB_BytePerSec:16;
-uint8_t BPB_SecPerClus;
+uint8_t BS_jmpBoot[3];//跳转到boot code
+uint8_t BS_OEMName[8];//代工厂名字?
+uint32_t BPB_BytePerSec:16;//每一个扇区的字节数
+uint8_t BPB_SecPerClus;//每一个簇的扇区数
 uint32_t BPB_RsvdSecCnt:16;
 uint8_t BPB_NumFATs;
 uint32_t BPB_RootEntCnt:16;
@@ -25,16 +25,16 @@ uint32_t BPB_FSVer:16;
 uint32_t BPB_RootClus;
 uint32_t BPB_FSinfo:16;
 uint32_t BPB_BkBootSec:16;
-uint8_t BPB_Reserved[8];
+uint8_t BPB_Reserved[12];
 uint8_t BS_DrvNum;
 uint8_t BS_Reserved1;
 uint8_t BS_BootSig;
-uint32_t BS_VollD;
-uint8_t BS_ValLab[11];
-uint8_t BS_FilSysType[8];
-uint8_t padding[420];
-uint32_t signature_word:16;
-} __attribute__((packed)) fat_header;
+uint32_t BS_VolID;
+uint8_t BS_VolLab[11];
+uint8_t BS_FilSysType[8];//字符串"FAT32"
+uint8_t padding[420];//补充空位
+uint32_t signature_word:16;//0x55AA
+}fat_header __attribute__((packed)) ;
 
 int main(int argc, char *argv[]) {
     printf("%d\n",(int)sizeof(fat_header));
