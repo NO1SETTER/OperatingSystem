63520533dd4d9e1e8c0ebfc30e6b893492b4d9c6
tracer-nju <tracer@nju.edu.cn>
2020-06-07 13:01:31 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: f999985a5309478ca2aae7c0ea62d336   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  13:01:31 up  4:32,  1 user,  load average: 2.31, 1.12, 0.58

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,2 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdint.h>
+
+typedef struct 
+{
+uint8_t BS_jmpBoot[3];
+uint8_t BS_OEMName[8];
+uint16_t a;
+}fat_header;
+
 int main(int argc, char *argv[]) {
 }
