82fb6453a09390632f50ff5eb195ccdce08fd2c6
tracer-nju <tracer@nju.edu.cn>
2020-06-07 08:57:40 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: f999985a5309478ca2aae7c0ea62d336   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  08:57:41 up 28 min,  1 user,  load average: 1.33, 1.52, 1.18

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -331,30 +331,30 @@ void sp_lockinit(spinlock_t* lk,const char *name)
 _Context* schedule(_Event ev,_Context* c)
 {
   #ifdef NO_STARVATION
-
-  if(current==NULL)
-  {
-    current=active_thread[0];
-  }
-  else//寻找最小线程是不是可以在activate和ｗait的时候进行?
-  {
-    current->ctx = c;
-    current=active_thread[0];
-  }
-  assert(current);
-  return current->ctx;
+      if(current==NULL)
+      {
+        current=active_thread[0];
+      }
+      else//寻找最小ct线程是不是可以在activate和ｗait的时候进行?
+      {
+        current->ctx = c;
+        current=active_thread[0];
+      }
+      current->ct=current->ct+1;
+      assert(current);
+    return current->ctx;
   #else
-  if(current==NULL)
-  {
-    current=active_thread[0];
-  }
-  else
-  {
-    current->ctx = c;
-    current = active_thread[rand()%active_num]; 
-  }
-  assert(current);
-  return current->ctx;
+      if(current==NULL)
+      {
+        current=active_thread[0];
+      }
+      else
+      {
+        current->ctx = c;
+        current = active_thread[rand()%active_num]; 
+      }
+      assert(current);
+      return current->ctx;
   #endif
 
 }
@@ -443,18 +443,18 @@ void activate(task_t* t,sem_t* sem)//wait->running
   wait_num=wait_num-1;
   
   #ifdef NO_STARVATION//NO_STARVATION状态下,调整为按ct升序排列
-  int pivot=0;//插入的位置
-  for(int i=0;i<active_num;i++)
-  {
-    if((active_thread[i]->ct)>=(t->ct)) break;
-    pivot=i;
-  }
-  for(int i=active_num;i>pivot;i++)
-  active_thread[i]=active_thread[i-1];
-  active_thread[pivot]=t;
-  active_num++;
+    int pivot=0;//插入的位置
+    for(int i=0;i<active_num;i++)
+    {
+      pivot=i;
+      if((active_thread[i]->ct)>=(t->ct)) break;
+    }
+    for(int i=active_num;i>pivot;i++)
+    active_thread[i]=active_thread[i-1];
+    active_thread[pivot]=t;
+    active_num++;
   #else
-  active_thread[active_num++]=t;
+    active_thread[active_num++]=t;
   #endif
   
   t->status=T_RUNNING;
