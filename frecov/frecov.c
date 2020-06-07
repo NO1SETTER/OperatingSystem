dd65f23baa6d957b8a37a49b01aa4145f0ace03e
tracer-nju <tracer@nju.edu.cn>
2020-06-07 13:20:22 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: f999985a5309478ca2aae7c0ea62d336   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  13:20:22 up  4:50,  1 user,  load average: 0.77, 0.66, 0.65

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -7,8 +7,36 @@ typedef struct
 {
 uint8_t BS_jmpBoot[3];
 uint8_t BS_OEMName[8];
-uint16_t a;
-}fat_header;
+uint32_t BPB_BytePerSec:16;
+uint8_t BPB_SecPerClus;
+uint32_t BPB_RsvdSecCnt:16;
+uint8_t BPB_NumFATs;
+uint32_t BPB_RootEntCnt:16;
+uint32_t BPB_TotSec16:16;
+uint8_t BPB_Media;
+uint32_t BPB_FATSz16:16;
+uint32_t BPB_SecPerTrk:16;
+uint32_t BPB_NumHeads:16;
+uint32_t BPB_HiddSec;
+uint32_t BPB_TotSec32;
+uint32_t BPB_FATSz32;
+uint32_t BPB_ExtFlags:16;
+uint32_t BPB_FSVer:16;
+uint32_t BPB_RootClus;
+uint32_t BPB_FSinfo:16;
+uint32_t BPB_BkBootSec:16;
+uint8_t BPB_Reserved[8];
+uint8_t BS_DrvNum;
+uint8_t BS_Reserved1;
+uint8_t BS_BootSig;
+uint32_t BS_VollD;
+uint8_t BS_ValLab[11];
+uint8_t BS_FilSysType[8];
+uint8_t padding[420];
+uint32_t signature_word:16;
+}fat_header __attribute__((packed));
 
 int main(int argc, char *argv[]) {
+assert(sizeof(fat_header)==512);
+
 }
