dea0e399b447f6b4a1355eb81afb20cbf085bc39
tracer-nju <tracer@nju.edu.cn>
2020-06-10 10:55:06 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: ec84a393e7124a3bb7565342c27316c9   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  10:55:06 up  2:15,  1 user,  load average: 1.51, 1.40, 1.24

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -115,6 +115,6 @@ int GetSize(char *fname)
       uint32_t sectors = header->BPB_TotSec32-header->BPB_RsvdSecCnt-header->BPB_FATSz32*header->BPB_NumFATs;
       DataClusters = sectors/header->BPB_SecPerClus;
       ClusterSize=header->BPB_SecPerClus*header->BPB_BytePerSec;
-      printf("Data Region has %x clusters\n",DataClusters);
-      printf("Clustersize is %x bytes\n",ClusterSize);
+      printf("Data Region has 0x%x clusters\n",DataClusters);
+      printf("Clustersize is 0x%x bytes\n",ClusterSize);
   }
