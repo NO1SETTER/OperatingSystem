2460504b2c596d9c04a30237c70bbcd89c410ca8
tracer-nju <tracer@nju.edu.cn>
2020-06-11 10:19:01 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 803e6ec0442c47d5979c298a075053c1   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-101-generic       Architecture: x86-64  10:19:01 up  1:47,  1 user,  load average: 1.74, 1.47, 1.43

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -124,6 +124,7 @@ for(int i=0;i<DataClusters;i++,cstart=cstart+ClusterSize)
   if(ctype[i]!=DIRECTORY_ENTRY) continue;
     printf("This is a directory_entry\n");
     struct sdir_entry* sdir=(struct sdir_entry*)cstart;
+    assert(sdir);
     if(sdir->DIR_Attr==ATTR_LONG_NAME)//变长文件头
     {
       assert(0);
