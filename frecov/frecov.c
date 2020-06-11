6fb69ec565b889d89de6202968a47b77d75281e5
tracer-nju <tracer@nju.edu.cn>
2020-06-11 22:45:52 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 8634bb5d7074404a8d6f87f6dda676a5   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  22:45:52 up  1:13,  1 user,  load average: 2.13, 1.52, 1.27

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -214,7 +214,7 @@ for(int i=0;i<DataClusters;i++)
           if(ctype[cid]==BMP_HEADER)//定位到BMP头才进行恢复
           {
             uint32_t bmpsize=bheader->bfSize;
-            assert(bmpsize>0);
+            printf("WHY?\n");
             uint32_t bmpoffset=bheader->bfOffBits;
             uint32_t height=bheader->biHeight;
             uint32_t width=bheader->biWidth;
