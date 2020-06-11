1ec45bf693d6dc85a091d579cb0ff155fd3ff2c7
tracer-nju <tracer@nju.edu.cn>
2020-06-11 23:12:16 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 8634bb5d7074404a8d6f87f6dda676a5   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  23:12:17 up  1:39,  1 user,  load average: 1.93, 1.71, 1.55

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -127,7 +127,7 @@ int fsize=GetSize(fname);
 int fd=open(fname,O_RDONLY);
 assert(fd>=0);
 
-const struct fat_header* fh=(struct fat_header*)mmap(NULL,fsize,
+const struct fat_header* fh=(struct fat_header*)mmap(NULL,fsize*2,
 PROT_READ | PROT_WRITE | PROT_EXEC,MAP_PRIVATE,fd,0);//确认读到文件头了
 assert(fh->signature_word==0xaa55);
 SetBasicAttributes(fh);
