0556583832b0c80b7dbd722af5a09d3fcac9104c
tracer-nju <tracer@nju.edu.cn>
2020-06-11 23:36:03 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 8634bb5d7074404a8d6f87f6dda676a5   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  23:36:03 up  2:03,  1 user,  load average: 1.29, 1.40, 1.42

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -218,8 +218,8 @@ for(int i=0;i<DataClusters;i++)
             printf("header at %p cstart at %p\n",header,header+ClusterSize*cid+DataOffset);
             
             char c=*(char*)bheader;
-            //assert(c);
-            assert(0);
+            assert(c);
+            if(c)
             printf("LOLING\n");
             assert(*((char*)bheader+1)=='M');
            
