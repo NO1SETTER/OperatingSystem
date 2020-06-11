3436ca7053e53f02d47c9720366ddcb97d2f0ad4
tracer-nju <tracer@nju.edu.cn>
2020-06-11 15:59:27 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: a9cf99b5c548439abeb5d0d5fe93a806   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  15:59:27 up 24 min,  1 user,  load average: 1.70, 1.46, 1.30

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -146,8 +146,8 @@ for(int i=0;i<DataClusters;i++)
     memset(name,'\0',sizeof(name));
     struct sdir_entry* sdir=(struct sdir_entry* )(cptr-8);
     struct ldir_entry* ldir=(struct ldir_entry* )(cptr-40);
-    //printf("sdir_entry at %x\n",(unsigned)((void *)sdir-header));
-    //printf("ldir_entry at %x\n",(unsigned)((void *)ldir-header));
+    printf("sdir_entry at %x\n",(unsigned)((void *)sdir-header));
+    printf("ldir_entry at %x\n",(unsigned)((void *)ldir-header));
     if(Chksum((unsigned char*)sdir)!=ldir->LDIR_Chksum)//未匹配成功:短文件名
     {
       char prefix[10];
@@ -162,7 +162,7 @@ for(int i=0;i<DataClusters;i++)
       assert(sdir->DIR_Name[6]=='~'&&sdir->DIR_Name[7]=='1'); 
       while(1)//提取每一个长文件目录项里的文件名Part
       {
-        //printf("ldir_entry at %x\n",(unsigned)((void *)ldir-header));
+        printf("ldir_entry at %x\n",(unsigned)((void *)ldir-header));
         char name_tp[25];
         int pos=0;
         int reachend=0;
@@ -190,7 +190,7 @@ for(int i=0;i<DataClusters;i++)
           else reachend=1;
         }
         strcat(name,name_tp);
-        //printf("name_tp : %s\n",name_tp);
+        printf("name_tp : %s\n",name_tp);
         if(reachend) break;
         ldir=ldir-1;
       }
