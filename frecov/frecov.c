8233be3dc62b3482b0de50076d3f6c0409f0b0d1
tracer-nju <tracer@nju.edu.cn>
2020-06-11 17:18:20 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: a9cf99b5c548439abeb5d0d5fe93a806   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  17:18:20 up  1:43,  1 user,  load average: 1.06, 1.20, 1.33

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -189,6 +189,7 @@ for(int i=0;i<DataClusters;i++)
           //this field:recover data
 
           uint32_t cid=((sdir->DIR_FstClusHI<<16)|sdir->DIR_FstClusLO);
+          if(ctype[cid]!=BMP_HEADER) printf("not\n");
           printf("First Cluster at cluster %d at %x\n",cid,ClusterSize*cid);
 
 
