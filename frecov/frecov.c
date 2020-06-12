d35852f13fcf11d1c1fd5405a2701c9dc2e26362
tracer-nju <tracer@nju.edu.cn>
2020-06-12 14:25:16 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 40b70578c1354ab8971123651e9096ea   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  14:25:16 up 4 min,  1 user,  load average: 1.04, 1.66, 0.84

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -219,7 +219,7 @@ for(int i=0;i<DataClusters;i++)
             printf("BMP Data Offset:%x\n",bheader->bfOffBits);
             //基本是0x36，也就是54字节，说明数据正好接在BMP_HEADER后面
             char tmpname[128];
-            sprintf(tmpname,"tmp/%s",name);
+            sprintf(tmpname,"/tmp/%s",name);
             FILE *fp=fopen(tmpname,"a+");
             assert(fp);
             fwrite((void *)bheader,1,sizeof(struct bitmap_header),fp);
