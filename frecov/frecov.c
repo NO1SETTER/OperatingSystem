0e0a5cc9b3e607b76d0ab984752e062259b11d5d
tracer-nju <tracer@nju.edu.cn>
2020-06-12 13:55:19 +0800

    Static hostname: ebata          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4d07f25c5a2a44c19b3706517d4e1451            Boot ID: 9aad632eef9047b99e807a9f8535798d   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  13:55:19 up  2:43,  1 user,  load average: 1.33, 1.21, 1.19

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -217,10 +217,9 @@ for(int i=0;i<DataClusters;i++)
             uint32_t height=bheader->biHeight;
             uint32_t width=bheader->biWidth;
             printf("BMP Data Offset:%x\n",bheader->bfOffBits);
-            //基本是0x36，也就是54字节，说明数据正好接在BMP头上
+            //基本是0x36，也就是54字节，说明数据正好接在BMP_HEADER后面
           }
           //this field:recover data
-          assert(0);
     cptr++;
   }
 }
